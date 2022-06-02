59be41ab4ad95a0f501ee8c9cf2ba9c4202ebe5f
fahmifayyadh <fahmifayyadh4@gmail.com>
2022-06-02 16:41:13 +0700

    admin pengguna fe & be done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = ([
-    'index', 'login', 'list-wisata', 'tambah-wisata', 'list-pengguna', 'tambah-pengguna',
+    'index', 'login', 'list-wisata', 'tambah-wisata', 'list-pengguna', 'tambah-pengguna', 'list-pengelola', 'tambah-pengelola'
 ]);
 
 app.engine('html', require('ejs').renderFile);
